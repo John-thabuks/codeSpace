3fb94b378b5b6f3e11bc62232b00627f0ee1c439
Ndindairene <irene.ndindaa@gmail.com>
2023-12-13 20:37:04 +0300

    RecipeCard

--- myrecipeapp/src/components/RecipeCard.js
+++ myrecipeapp/src/components/RecipeCard.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import RecipeDetailsModal from "./RecipeDetailsModal"
+import React from 'react';
+import RecipeDetailsModal from './RecipeDetailsModal';
 
-function RecipeCard() {
-    return (
-        <RecipeDetailsModal />
-    )
-}
+const RecipeCard = ({ recipe }) => {
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
 
-export default RecipeCard
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className="recipe-card" onClick={openModal}>
+      <h3>{recipe.title}</h3>
+      {/* Display other recipe details */}
+      {isModalOpen && <RecipeDetailsModal recipe={recipe} closeModal={closeModal} />}
+    </div>
+  );
+};
+
+export default RecipeCard;
